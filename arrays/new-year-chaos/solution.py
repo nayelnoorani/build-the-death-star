5f0ca81f6a54bdda700beb732c9356cbd085156e
Nayel Noorani <nayel.noorani@gmail.com>
2024-09-18 23:10:56 -0400

    corrected minimum bribes

--- arrays/new-year-chaos/solution.py
+++ arrays/new-year-chaos/solution.py
@@ -1,13 +1,21 @@
 def minimumBribes(q):
     # Write your code here
     bribes = 0
-    if q == sorted(q):
-        pass
-    else:
-        for i, person in enumerate(q):
-            if person - i > 3:
-                print("Too chaotic")
-                return None
-            elif person - i > 1:
-                bribes += person - (i + 1)
+
+    # Iterate over the queue from right to left
+    for i in range(len(q) -1, -1, -1):
+        # Check if the person has moved more than 2 positions ahead
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        
+        # Count how many times this person was bribed by checking
+        # the positions from max(0, q[i] -2) to the current position
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribes += 1
+
     print(bribes)
+
+q = [1, 2, 5, 3, 7, 8, 6, 4]
+minimumBribes(q)
